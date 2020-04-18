519996eddcb6baee5be839783c8889880643207b
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-18 23:25:57 +0530

    initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,11 @@
+import React, { Component } from "./react";
+
+class Todo extends Component {
+  render() {
+    return (
+      <div className="Todo">
+        <h1>Todo</h1>
+      </div>
+    );
+  }
+}
