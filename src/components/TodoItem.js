c8baf81112165175093f003695103262f3ebfe2a
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-19 07:40:09 +0530

    adding functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
+
   render() {
-    console.log(this.props);
-    return <h3>{this.props.todo.title}</h3>;
+    return (
+      <div>
+        <input type="checkbox" onChange={this.props.complete} />
+        <p style={this.getStyle()}>{this.props.todo.title}</p>
+      </div>
+    );
   }
 }
+
+TodoItem.propType = {
+  todo: PropTypes.object.isRequired,
+};
 
 export default TodoItem;
